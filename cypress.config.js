a20300b5ca79902adbf1b74e118d8b91071ac89e
absiddik7 <abubakkar1070@gmail.com>
2023-09-25 17:17:23 +0600

    report updated

--- cypress.config.js
+++ cypress.config.js
@@ -2,19 +2,15 @@ const { defineConfig } = require("cypress");
 const cucumber = require('cypress-cucumber-preprocessor').default 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    charts: true,
-    reportPageTitle: 'custom-title',
-    embeddedScreenshots: true,
-    inlineAssets: true,
-    saveAllAttempts: false,
-  },
+  
+
   e2e: {
     setupNodeEvents(on, config) {
       on('file:preprocessor', cucumber());
       require('cypress-mochawesome-reporter/plugin')(on);
-      this.screenshotOnRunFailure = true;
+      
     },
+
     specPattern: "**/*.feature",
   },
 });
