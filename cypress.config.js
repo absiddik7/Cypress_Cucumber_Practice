68149d94bd7e0786ae52d01017dfe4ebb4a76b2f
absiddik7 <abubakkar1070@gmail.com>
2023-09-25 15:48:47 +0600

    new update

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,12 @@
 const { defineConfig } = require("cypress");
 const cucumber = require('cypress-cucumber-preprocessor').default 
 module.exports = defineConfig({
+  video: true,
+  "reporter": "cypress-multi-reporters",
+    "reporterOptions": {
+        "configFile": "reporter-config.json"
+    },
+    "videoCompression": 15,
   e2e: {
     setupNodeEvents(on, config) {
       on('file:preprocessor', cucumber())
