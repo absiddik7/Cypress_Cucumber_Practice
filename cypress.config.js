16ab3f98e2b528bc0e66dcba6c72c4b9abebbce1
absiddik7 <abubakkar1070@gmail.com>
2023-09-25 16:11:19 +0600

    update

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,19 @@
 const { defineConfig } = require("cypress");
 const cucumber = require('cypress-cucumber-preprocessor').default 
 module.exports = defineConfig({
-  video: true,
-  "reporter": "cypress-multi-reporters",
-    "reporterOptions": {
-        "configFile": "reporter-config.json"
-    },
-    "videoCompression": 15,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      on('file:preprocessor', cucumber())
+      on('file:preprocessor', cucumber());
+      require('cypress-mochawesome-reporter/plugin')(on);
+      this.screenshotOnRunFailure = true;
     },
     specPattern: "**/*.feature",
   },
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,16 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
-import addContext from 'mochawesome/addContext'
-
-Cypress.on("test:after:run", (test, runnable) => {
-    
-    let videoName = Cypress.spec.name
-    videoName = videoName.replace('/.js.*', '.js')
-    const videoUrl = 'videos/' + videoName + '.mp4'
-
-    addContext({ test }, videoUrl)
-});
+import 'cypress-mochawesome-reporter/register';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
