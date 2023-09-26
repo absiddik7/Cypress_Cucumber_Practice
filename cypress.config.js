2d153f1bd7ca2bcf26e25f6c97575b5fa915f735
absiddik7 <abubakkar1070@gmail.com>
2023-09-26 11:11:09 +0600

    update

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   "reporterOptions": {
     "reportDir": "cypress\reports\html", // Directory where reports will be generated
-    "overwrite": false, // Set to true to overwrite existing reports (optional)
+    "overwrite": true, // Set to true to overwrite existing reports (optional)
     "html": true, // Generate HTML report
     "json": true // Generate JSON report (optional)
   },
