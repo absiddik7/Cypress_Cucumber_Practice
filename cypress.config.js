201b2ce5a61ca2fef208ec62f51266dfd023eaa8
absiddik7 <abubakkar1070@gmail.com>
2023-09-26 10:53:34 +0600

    updated

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,12 @@ const { defineConfig } = require("cypress");
 const cucumber = require('cypress-cucumber-preprocessor').default 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
-  
+  "reporterOptions": {
+    "reportDir": "cypress\reports\html", // Directory where reports will be generated
+    "overwrite": false, // Set to true to overwrite existing reports (optional)
+    "html": true, // Generate HTML report
+    "json": true // Generate JSON report (optional)
+  },
 
   e2e: {
     setupNodeEvents(on, config) {
