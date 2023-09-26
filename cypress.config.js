cca9da169ec90b53edd39c52e9a30506524e0222
absiddik7 <abubakkar1070@gmail.com>
2023-09-26 11:34:32 +0600

    updated

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,30 @@
 const { defineConfig } = require("cypress");
 const cucumber = require('cypress-cucumber-preprocessor').default 
+const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   "reporterOptions": {
-    "reportDir": "cypress\reports\html", // Directory where reports will be generated
-    "overwrite": true, // Set to true to overwrite existing reports (optional)
-    "html": true, // Generate HTML report
-    "json": true // Generate JSON report (optional)
+    // reportDir: "cypress\reports\html", // Directory where reports will be generated
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    overwrite: true, // Set to true to overwrite existing reports (optional)
+    html: true, // Generate HTML report
+    json: true, // Generate JSON report (optional)
   },
 
   e2e: {
     setupNodeEvents(on, config) {
       on('file:preprocessor', cucumber());
-      require('cypress-mochawesome-reporter/plugin')(on);
+      
+      on('before:run', async (details) => {
+        console.log('override before:run');
+        await beforeRunHook(details);
+      });
+
+      on('after:run', async () => {
+        console.log('override after:run');
+        await afterRunHook();
+      });
       
     },
 
