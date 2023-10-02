a43301e70778f5e244b1674cbb54d25d16227da9
absiddik7 <abubakkar1070@gmail.com>
2023-10-02 12:41:36 +0600

    new test added

--- cypress/e2e/step_definations/login.js
+++ cypress/e2e/step_definations/login.js
@@ -1,46 +1,56 @@
-import { Given, When, Then, And, Scenario } from "cypress-cucumber-preprocessor/steps";
+import {
+  Given,
+  When,
+  Then,
+  And,
+  Scenario,
+} from "cypress-cucumber-preprocessor/steps";
 
-Cypress.on('uncaught:exception', (err, runnable) => {
-    return false
-})
-
-Given('I visit the login page', () => {
-    cy.visit('https://practicetestautomation.com/practice-test-login/')
+Cypress.on("uncaught:exception", (err, runnable) => {
+  return false;
 });
 
-//it("Login with valid credentials", () => {
-When('I enter valid username and password', () => {
-    cy.get('input[name="username"]').type("student")
-    cy.get('input[name="password"]').type("Password123")
+Given("I visit the login page", () => {
+  cy.visit("https://practicetestautomation.com/practice-test-login/");
 });
 
-And('clicks on the login button', () => {
-    cy.get('button[id="submit"]').click()
+// Validate login with valid credentials
+it("Login with valid credentials", () => {
+  When(
+    "I enter valid username {string} and password {string}",
+    (username, password) => {
+      cy.get('input[name="username"]').type(username);
+      cy.get('input[name="password"]').type(password);
+    }
+  );
+
+  And("clicks on the login button", () => {
+    cy.get('button[id="submit"]').click();
+  });
+
+  Then("I see successful login message", () => {
+    cy.get("h1").should("have.text", "Logged In Successfully");
+  });
 });
 
-Then('I see successful login message', () => {
-    cy.get('h1').should('have.text', 'Logged In Successfully')
+// Validate login with invalid credentials
+it("Login with invalid username", () => {
+  When(
+    "I enter invalid username {string} and valid password {string}",
+    (username, password) => {
+      cy.get('input[name="username"]').type(username);
+      cy.get('input[name="password"]').type(password);
+    }
+  );
+
+  And("clicks on the login button", () => {
+    cy.get('button[id="submit"]').click();
+  });
+
+  Then("I see unsuccessful login message", () => {
+    cy.get('div[id = "error"]').should(
+      "have.text",
+      "Your username is invalid!"
+    );
+  });
 });
-//});
-
-
-
- 
-//it("Login with invalid username", () => {
-When('I enter invalid username and valid password', () => {
-    cy.get('input[name="username"]').type("incorrectUser")
-    cy.get('input[name="password"]').type("Password1234")
-});
-
-And('clicks on the login button', () => {
-    cy.get('button[id="submit"]').click()
-});
-
-Then('I see unsuccessful login message', () => {
-    cy.get('div[id = "error"]').should('have.text', 'Your username is invalid!4')
-});
-
-//});
-
-
-
